92139bff16e96119acdfaec6746b21971b255f47
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-08-15 17:36:19 +0300

    feat: add functionality to signup user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,22 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, HttpCode, Post } from "@nestjs/common";
 import { AuthDto } from './dto/auth.dto';
+import { AuthService } from "./auth.service";
+import { ALREADY_REGISTERED_ERROR } from "./constants/auth.constants";
 
 @Controller('auth')
 export class AuthController {
-    @Post('register')
-    async register(@Body() dto: AuthDto) {}
+    constructor(private readonly authService: AuthService) { }
+
+    @Post('signup')
+    async signup(@Body() dto: AuthDto) {
+        const oldUser = await this.authService.findUser(dto.login);
+
+        if (oldUser) {
+            throw new BadRequestException(ALREADY_REGISTERED_ERROR);
+        }
+
+        return this.authService.createUser(dto);
+    }
 
     @HttpCode(200)
     @Post('login')
