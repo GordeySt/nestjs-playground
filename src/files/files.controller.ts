c89e2887736d286666239ca42840b548d21439e1
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-09-07 18:30:08 +0300

    feat: add functionality to convert images to webp

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -2,6 +2,7 @@ import { Controller, HttpCode, Post, UploadedFile, UseInterceptors } from "@nest
 import { FileInterceptor } from "@nestjs/platform-express";
 import { FileResponse } from "./dto/file.response";
 import { FilesService } from "./files.service";
+import { MFile } from "./mfile.class";
 
 @Controller('files')
 export class FilesController {
@@ -11,6 +12,16 @@ export class FilesController {
   @HttpCode(200)
   @UseInterceptors(FileInterceptor('files'))
   async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<FileResponse[]> {
-    return this.filesService.saveFiles([file]);
+    const saveArray: MFile[] = [new MFile(file)];
+
+    if (file.mimetype.includes('image')) {
+      const buffer = await this.filesService.convertToWebp(file.buffer);
+      saveArray.push(new MFile({
+        originalname: `${file.originalname.split('.')[0]}.webp`,
+        buffer
+      }))
+    }
+
+    return this.filesService.saveFiles(saveArray);
   }
 }
